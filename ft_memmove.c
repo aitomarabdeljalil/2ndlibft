cbcead8a0195a6595a0448fb67a7b042fe58722a
Soufiane Yakoubi <syakoubi@e2r6p9.1337.ma>
2021-11-05 10:22:13 +0100

    ft_memmove: use unsigned char to read bytes

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: syakoubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:08:29 by syakoubi          #+#    #+#             */
-/*   Updated: 2021/11/04 19:42:51 by syakoubi         ###   ########.fr       */
+/*   Updated: 2021/11/05 10:20:51 by syakoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char		*_dst;
-	const char	*_src;
+	unsigned char		*_dst;
+	const unsigned char	*_src;
 
 	_dst = dst;
 	_src = src;
