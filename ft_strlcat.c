9c98fecb4737624b1666ae1edf48075fc46427ce
Soufiane Yakoubi <syakoubi@e2r6p9.1337.ma>
2021-11-05 12:46:39 +0100

    ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syakoubi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 12:45:27 by syakoubi          #+#    #+#             */
+/*   Updated: 2021/11/05 12:45:29 by syakoubi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	len_dst;
+
+	i = 0;
+	while (dst[i] != '\0' && i < dstsize)
+		i++;
+	if (i == dstsize)
+		return (i + ft_strlen(src));
+	len_dst = i;
+	j = 0;
+	while (src[j] != '\0' && i < dstsize - 1)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (len_dst + ft_strlen(src));
+}
