d59bf1a0888443fcbf14ff8af06948d50e7396b3
Soufiane Yakoubi <syakoubi@e2r6p9.1337.ma>
2021-11-06 12:23:40 +0100

    ft_memcmp: don't treat '\0' as a special value

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: syakoubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 15:09:14 by syakoubi          #+#    #+#             */
-/*   Updated: 2021/11/04 17:26:12 by syakoubi         ###   ########.fr       */
+/*   Updated: 2021/11/06 12:14:14 by syakoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	_s1 = s1;
 	_s2 = s2;
-	while (_s1[i] && _s2[i] && _s1[i] == _s2[i] && i < n - 1)
+	while (_s1[i] == _s2[i] && i < n - 1)
 		i++;
 	return (_s1[i] - _s2[i]);
 }
