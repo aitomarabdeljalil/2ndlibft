1d4b8d0244f7f2275d2fa83a71fedffaa4917050
Soufiane Yakoubi <syakoubi@e2r6p9.1337.ma>
2021-11-06 12:34:57 +0100

    ft_strnstr: fix and simplify logic

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: syakoubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 12:58:39 by syakoubi          #+#    #+#             */
-/*   Updated: 2021/11/05 21:38:54 by syakoubi         ###   ########.fr       */
+/*   Updated: 2021/11/06 12:32:30 by syakoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	const char	*cursor;
 	int			len_needle;
+	size_t		i;
 
 	len_needle = ft_strlen(needle);
 	if (len_needle == 0)
 		return ((char *)haystack);
-	cursor = ft_strchr(haystack, needle[0]);
-	while (haystack != NULL && cursor <= haystack + len - len_needle)
+	i = 0;
+	while (haystack[i] && i <= len - len_needle)
 	{
-		if (ft_strncmp(haystack, needle, len_needle) == 0)
-			return ((char *)haystack);
-		haystack = ft_strchr(haystack + 1, needle[0]);
+		if (ft_strncmp(haystack + i, needle, len_needle) == 0)
+			return ((char *)haystack + i);
+		i++;
 	}
 	return (NULL);
 }
