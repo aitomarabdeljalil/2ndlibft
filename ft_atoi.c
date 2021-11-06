a0606b750135121343659ac4f49cca2478e34db7
Soufiane Yakoubi <syakoubi@e2r6p9.1337.ma>
2021-11-06 10:31:14 +0100

    ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syakoubi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/06 10:10:23 by syakoubi          #+#    #+#             */
+/*   Updated: 2021/11/06 10:33:09 by syakoubi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(int c)
+{
+	return ((c >= 9 && c <= 13) || c == 32);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	if (*str == '+')
+		str++;
+	while (ft_isdigit(*str))
+		result = result * 10 + (*(str++) - 48) * sign;
+	return (result);
+}
