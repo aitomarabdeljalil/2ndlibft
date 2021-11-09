ea109ef89a9358a054b033fa1f5d2d5482072961
Soufiane Yakoubi <syakoubi@e2r7p9.1337.ma>
2021-11-09 21:13:12 +0100

    ft_itoa

---
+++ ft_itoa.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syakoubi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 18:06:32 by syakoubi          #+#    #+#             */
+/*   Updated: 2021/11/09 21:09:55 by syakoubi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdbool.h>
+#include "libft.h"
+
+// doesn't work for INT_MIN
+int	ft_abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
+
+char	*ft_itoa(int n)
+{
+	char	tmp[12];
+	char	*cursor;
+	bool	neg;
+
+	cursor = tmp + 11;
+	*cursor = '\0';
+	neg = n < 0;
+	if (n == 0)
+		*(--cursor) = '0';
+	while (n)
+	{
+		*(--cursor) = ft_abs(n % 10) + 48;
+		n /= 10;
+	}
+	if (neg)
+		*(--cursor) = '-';
+	return (ft_strdup(cursor));
+}
