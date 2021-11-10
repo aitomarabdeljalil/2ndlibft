2e977ce16f065fd2dfff7c84c91eef1e5f5e2632
Soufiane Yakoubi <syakoubi@e2r7p9.1337.ma>
2021-11-10 17:32:13 +0100

    ft_putstr_fd: use less calls to write

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,14 +6,18 @@
 /*   By: syakoubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 16:44:04 by syakoubi          #+#    #+#             */
-/*   Updated: 2021/11/09 16:44:06 by syakoubi         ###   ########.fr       */
+/*   Updated: 2021/11/10 17:29:56 by syakoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "libft.h"
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	while (*s)
-		write(fd, s++, 1);
+	size_t	len;
+
+	len = ft_strlen(s);
+	if (len)
+		write(fd, s, len);
 }
