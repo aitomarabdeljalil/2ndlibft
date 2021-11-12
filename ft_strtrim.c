d0af875d85269d2f5a2882c0cf5acaacc87be342
Soufiane Yakoubi <syakoubi@e2r8p10.1337.ma>
2021-11-12 15:39:11 +0100

    ft_strtrim: use i instead of len

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: syakoubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 17:47:21 by syakoubi          #+#    #+#             */
-/*   Updated: 2021/11/06 17:48:02 by syakoubi         ###   ########.fr       */
+/*   Updated: 2021/11/12 15:18:27 by syakoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	len;
+	size_t	i;
 
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
-	len = ft_strlen(s1);
-	while (len && ft_strchr(set, s1[len]))
-		len--;
-	return (ft_substr(s1, 0, len + 1));
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, s1[i]))
+		i--;
+	return (ft_substr(s1, 0, i + 1));
 }
