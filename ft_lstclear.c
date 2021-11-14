dae3173d330d37342d6b516354e8c911e7b68c21
Soufiane Yakoubi <syakoubi@e2r8p10.1337.ma>
2021-11-14 12:43:44 +0100

    ft_lstclear: fix segmentation fault

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: syakoubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 14:35:26 by syakoubi          #+#    #+#             */
-/*   Updated: 2021/11/10 14:54:46 by syakoubi         ###   ########.fr       */
+/*   Updated: 2021/11/14 12:43:02 by syakoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list	*to_clear;
+
 	while (*lst)
 	{
-		ft_lstdelone(*lst, del);
+		to_clear = *lst;
 		*lst = (*lst)->next;
+		ft_lstdelone(to_clear, del);
 	}
 }
